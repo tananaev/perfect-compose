441eebdf19a7710b8b5f65f57d6379121209ffef
Anton Tananaev <atananaev@lyft.com>
2020-12-16 13:13:07 -0800

    Implement attach detach

--- app/src/main/java/com/tananaev/perfectcompose/increment/IncrementInteractor.kt
+++ app/src/main/java/com/tananaev/perfectcompose/increment/IncrementInteractor.kt
@@ -1,5 +1,7 @@
 package com.tananaev.perfectcompose.increment
 
+import android.util.Log
+import androidx.compose.runtime.CompositionLifecycleObserver
 import com.tananaev.perfectcompose.Action
 import com.tananaev.perfectcompose.Store
 import javax.inject.Inject
@@ -10,7 +12,7 @@ object ActionIncrement : Action
 
 class IncrementInteractor @Inject constructor(
     private val incrementService: IncrementService,
-) {
+) : CompositionLifecycleObserver {
     private val store = Store(IncrementState(0), ::reduce)
 
     private fun reduce(state: IncrementState, action: Action): IncrementState {
@@ -28,4 +30,12 @@ class IncrementInteractor @Inject constructor(
     fun initialState() = store.initialState
 
     fun observeState() = store.observeState()
+
+    override fun onEnter() {
+        Log.d(IncrementInteractor::class.simpleName, "attached")
+    }
+
+    override fun onLeave() {
+        Log.d(IncrementInteractor::class.simpleName, "detached")
+    }
 }
